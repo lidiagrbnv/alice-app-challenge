f0f5242ed0430bc139bc33b4474ca224c4755f57
lidos <21261008+snavr@users.noreply.github.com>
2021-07-23 11:34:16 +0200

    feat: caluculating portfolio

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -19,12 +19,25 @@ const Portfolio = () => {
   //@ts-ignore
   const portfolioWeights = portfolios[risk]
   const stockNames = portfolioWeights.map((stock: any) => stock.ticker).join(',')
+  const monthlyContribution  = Number(context.income) * 0.15
 
-  console.log(stockNames)
+  console.log(monthlyContribution)
+
+  const investmentPortfolio = portfolioWeights.reduce((acc: any, cur: any) => ({...acc, [cur.ticker]: Number(cur.weight) * monthlyContribution}), {})
+
+  console.log(investmentPortfolio)
 
   const { stockHistory, isLoading, isError } = useStockHistory(stockNames)
 
   console.log(stockHistory)
+
+  const calculateFinalStockNumber = Object.keys(stockHistory).map((item) => (
+    stockHistory[item].reduce((acc: any, curr: any) => {
+      const shares = +(investmentPortfolio[item] / Number(curr.close)).toFixed(2)
+
+      console.log(shares)
+    }, {})
+  ))
 
   return (
     <h1>Hello</h1>
--- src/components/portfolio/resources.ts
+++ src/components/portfolio/resources.ts
@@ -1,19 +1,46 @@
+import { AnyRecord } from 'dns'
 import useSWR from 'swr'
 
 const APIKey = 'c3003754ffd2dd059d005fa6c9709e2e'
 const URL = 'https://financialmodelingprep.com/api/v3'
 const from = '2017-01-01'
 const to = '2021-06-01'
-const query = `apikey=${APIKey}&from=${from}&to=${to}`
+const query = {
+  apikey: APIKey,
+  from,
+  to
+}
+
+const sortStockHistory = (stockList: any) => {
+  if (!stockList) return
+
+  return (
+    stockList.reduce((acc: any, stock: any) => {
+    const dates = stock.historical.filter((item: any, idx: any) => {
+      const dayOfMonth = new Date(item.date).getDate()
+      
+      return idx === 0 || dayOfMonth === 1
+    }).map(({ date, close }: any) => ({
+      date,
+      close,
+    }))
+    
+    return ({
+      ...acc,
+      [stock.symbol]: dates,
+    })
+  }, {})
+  )
+}
 
 //@ts-ignore
 const fetcher = (...args: any) => fetch(...args).then(res => res.json())
 
 export const useStockHistory = (portfolio: string) => {
-  const { data, error } = useSWR(`${URL}/historical-price-full/${portfolio}?${query}`, fetcher)
+  const { data, error } = useSWR(`${URL}/historical-price-full/${portfolio}?${new URLSearchParams(query)}`, fetcher)
 
   return {
-    stockHistory: data,
+    stockHistory: sortStockHistory(data?.historicalStockList),
     isLoading: !error && !data,
     isError: error
   }
